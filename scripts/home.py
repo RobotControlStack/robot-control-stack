bac55f46276c96e330ba28f155b4942ef5ee0cb2
Tobias Jülg <tobias.juelg@utn.de>
2024-01-26 16:37:15 +0100

    Added home script

---
+++ scripts/home.py
@@ -0,0 +1,18 @@
+import argparse
+import pyfr3
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Tool to move the FR3 to defined position."
+    )
+    parser.add_argument(
+        "ip",
+        type=str,
+        help="IP of the robot"
+    )
+    args = parser.parse_args()
+
+    f = pyfr3.FR3(args.ip, "models/panda.urdf")
+    g = pyfr3.FrankaHand(args.ip)
+    g.shut()
+    f.move_home()
