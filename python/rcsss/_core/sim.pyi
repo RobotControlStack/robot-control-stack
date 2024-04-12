5ffce808146bf5a1531a8ac653b3f514909eaa06
Pierre Krack <pierre.krack@utn.de>
2024-04-12 12:56:09 +0200

    Update stub files

--- python/rcsss/_core/sim.pyi
+++ python/rcsss/_core/sim.pyi
@@ -4,18 +4,23 @@ sim module
 """
 from __future__ import annotations
 
+import typing
+
 import rcsss._core.common
 
-__all__ = ["FR3", "FR3Config", "FR3Load", "FR3State"]
+__all__ = ["FR3", "FR3Config", "FR3State"]
 
 class FR3(rcsss._core.common.Robot):
-    def __init__(self, mjmdl: str, rlmdl: str) -> None: ...
+    def __init__(self, mjmdl: str, rlmdl: str, render: bool | None) -> None: ...
+    def reset(self) -> None: ...
 
-class FR3Config:
-    pass
+class FR3Config(rcsss._core.common.RConfig):
+    ik_duration: int
+    def __init__(self) -> None: ...
 
-class FR3Load:
-    pass
-
-class FR3State:
-    pass
+class FR3State(rcsss._core.common.RState):
+    def __init__(self) -> None: ...
+    @property
+    def collision(self) -> bool: ...
+    @property
+    def ik_success(self) -> bool: ...
