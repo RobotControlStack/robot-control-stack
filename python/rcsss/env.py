9060da359c6e639f8bf7656e5fe0e176c73a8481
Pierre Krack <pierre.krack@utn.de>
2024-04-16 11:51:29 +0200

    formatting

--- python/rcsss/env.py
+++ python/rcsss/env.py
@@ -8,12 +8,10 @@ import numpy as np
 from rcsss import sim
 
 RPY = gym.spaces.Box(low=np.deg2rad(-180), high=np.deg2rad(180), shape=(3,))
-xyz = gym.spaces.Box(low=np.array(
-    [-855, -855, 0]), high=np.array([855, 855, 1188]), shape=(3,))
+xyz = gym.spaces.Box(low=np.array([-855, -855, 0]), high=np.array([855, 855, 1188]), shape=(3,))
 pose = gym.spaces.Dict({"rpy": RPY, "xyz": xyz})
 angles = gym.spaces.Box(
-    low=np.array([-2.3093, -1.5133, -2.4937, -
-                 2.7478, -2.4800, 0.8521, -2.6895]),
+    low=np.array([-2.3093, -1.5133, -2.4937, -2.7478, -2.4800, 0.8521, -2.6895]),
     high=np.array([2.3093, 1.5133, 2.4937, -0.4461, 2.4800, 4.2094, 2.6895]),
     dtype=np.float32,
     shape=(7,),
@@ -46,8 +44,7 @@ class FR3Base(gym.Env):
         rpy = pose.rotation_rpy()
         xyz = pose.translation()
         return OrderedDict(
-            pose=OrderedDict(rpy=np.array(
-                [rpy.roll, rpy.pitch, rpy.yaw]), xyz=np.array(xyz)),
+            pose=OrderedDict(rpy=np.array([rpy.roll, rpy.pitch, rpy.yaw]), xyz=np.array(xyz)),
             angles=self.robot.get_joint_position(),
             collision=state.collision,
         )
@@ -56,9 +53,7 @@ class FR3Base(gym.Env):
         self.robot.set_joint_position(act)
         return self._get_obs(), 0, False, False, {}
 
-    def reset(
-        self, seed: Optional[int] = None, options: Optional[dict[str, Any]] = None
-    ) -> tuple[Any, dict[str, Any]]:
+    def reset(self, seed: Optional[int] = None, options: Optional[dict[str, Any]] = None) -> tuple[Any, dict[str, Any]]:
         if seed is not None:
             msg = "seeding not implemented yet"
             raise NotImplementedError(msg)
@@ -70,12 +65,10 @@ class FR3Base(gym.Env):
 
 
 if __name__ == "__main__":
-    env = FR3Base("models/mjcf/scene.xml",
-                  "models/urdf/fr3_from_panda.urdf", render=False)
+    env = FR3Base("models/mjcf/scene.xml", "models/urdf/fr3_from_panda.urdf", render=False)
     obs = env.reset()
     # print(f"Initial obs: {obs}")
     act = env.action_space.sample()
     # print(f"Initial action: {act}")
-    obs, reward, terminated, truncated, info = env.step(
-        env.action_space.sample())
+    obs, reward, terminated, truncated, info = env.step(env.action_space.sample())
     # print(f"New obs: {obs}")
