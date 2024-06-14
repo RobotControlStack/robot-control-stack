870b5d137ee5c1e2fcab54634738f93e0e2ccf0f
Pierre Krack <pierre.krack@utn.de>
2024-06-14 10:36:03 +0200

    sim::FR3 callbacks

--- src/sim/FR3.cpp
+++ src/sim/FR3.cpp
@@ -19,24 +19,9 @@
 #include "rl/mdl/UrdfFactory.h"
 
 struct {
-  std::vector<std::string> arm = {"link0_c", "link1_c",  "link2_c",  "link3_c",
-                                  "link4_c", "link5_c0", "link5_c1", "link5_c2",
-                                  "link6_c", "link7_c"};
-  std::vector<std::string> hand = {"hand_c"};
-  std::vector<std::string> gripper = {
-      "finger_0_left",
-      "fingertip_pad_collision_1_left",
-      "fingertip_pad_collision_2_left",
-      "fingertip_pad_collision_3_left",
-      "fingertip_pad_collision_4_left",
-      "fingertip_pad_collision_5_left",
-      "finger_0_right",
-      "fingertip_pad_collision_1_right",
-      "fingertip_pad_collision_2_right",
-      "fingertip_pad_collision_3_right",
-      "fingertip_pad_collision_4_right",
-      "fingertip_pad_collision_5_right",
-  };
+  std::array<std::string, 10> arm_collision_geoms{
+      "link0_c",  "link1_c",  "link2_c",  "link3_c", "link4_c",
+      "link5_c0", "link5_c1", "link5_c2", "link6_c", "link7_c"};
   std::array<std::string, 7> joints = {
       "fr3_joint1", "fr3_joint2", "fr3_joint3", "fr3_joint4",
       "fr3_joint5", "fr3_joint6", "fr3_joint7",
@@ -47,52 +32,43 @@ struct {
   };
 } const model_names;
 
-static void init_geom_ids(std::set<size_t>& ids,
-                          std::vector<std::string> const& names,
-                          mjModel const& m, const std::string& id) {
-  for (auto const& name : names) {
-    std::string name_with_id = name;
-    name_with_id.append("_");
-    name_with_id.append(id);
-    int id = mj_name2id(&m, mjOBJ_GEOM, name_with_id.c_str());
-    if (id == -1)
-      throw std::runtime_error(std::string("No geom named ") + name_with_id);
-    ids.insert(id);
-  }
-}
-
-template <typename T>
-std::set<T> set_union(const std::set<T>& a, const std::set<T>& b) {
-  std::set<T> result = a;
-  result.insert(b.begin(), b.end());
-  return result;
-}
-
 namespace rcs {
 namespace sim {
 
 FR3::FR3(Sim sim, std::string& id, std::shared_ptr<rl::mdl::Model> rlmdl)
-    : sim{sim},
-      id{id},
-      rl{.mdl = rlmdl,
-         .kin = std::dynamic_pointer_cast<rl::mdl::Kinematic>(rlmdl)},
-      cfg{},
-      state{} {
+    : sim{sim}, id{id}, rl{.mdl = rlmdl}, cfg{}, state{} {
+  this->rl.kin = std::dynamic_pointer_cast<rl::mdl::Kinematic>(rlmdl);
   this->rl.ik =
       std::make_shared<rl::mdl::JacobianInverseKinematics>(this->rl.kin.get());
-  this->rl.ik->setDuration(std::chrono::milliseconds(this->cfg.ik_duration));
+  this->rl.ik->setDuration(
+      std::chrono::milliseconds(this->cfg.ik_duration_in_milliseconds));
   this->init_ids();
+  this->sim.register_cb(std::bind(&FR3::collision_callback, this),
+                        this->cfg.seconds_between_callbacks);
+  this->sim.register_cb(std::bind(&FR3::is_arrived_callback, this),
+                        this->cfg.seconds_between_callbacks);
+  this->sim.register_cb(std::bind(&FR3::is_moving_callback, this),
+                        this->cfg.seconds_between_callbacks);
+  this->sim.register_convergence_cb(std::bind(&FR3::convergence_callback, this),
+                                    this->cfg.seconds_between_callbacks);
   this->reset();
 }
 
 void FR3::init_ids() {
+  std::string name;
   // Collision geoms
-  init_geom_ids(this->ids.cgeom.arm, model_names.arm, *this->sim.m, this->id);
-  init_geom_ids(this->ids.cgeom.hand, model_names.hand, *this->sim.m, this->id);
-  init_geom_ids(this->ids.cgeom.gripper, model_names.gripper, *this->sim.m,
-                this->id);
+  for (size_t i = 0; i < std::size(model_names.arm_collision_geoms); ++i) {
+    name = model_names.arm_collision_geoms[i];
+    name.append("_");
+    name.append(this->id);
+    auto id = mj_name2id(this->sim.m, mjOBJ_GEOM, name.c_str());
+    if (id == -1) {
+      throw std::runtime_error(std::string("No geom named " + name));
+    }
+    this->ids.cgeom.insert(id);
+  }
   // Sites
-  std::string name = "attachment_site_";
+  name = "attachment_site_";
   name.append(this->id);
   this->ids.attachment_site = mj_name2id(this->sim.m, mjOBJ_SITE, name.c_str());
   if (this->ids.attachment_site == -1) {
@@ -122,7 +98,8 @@ void FR3::init_ids() {
 
 bool FR3::set_parameters(const FR3Config& cfg) {
   this->cfg = cfg;
-  this->rl.ik->setDuration(std::chrono::milliseconds(this->cfg.ik_duration));
+  this->rl.ik->setDuration(
+      std::chrono::milliseconds(this->cfg.ik_duration_in_milliseconds));
   return true;
 }
 
@@ -148,14 +125,15 @@ common::Pose FR3::get_cartesian_position() {
 }
 
 void FR3::set_joint_position(const common::Vector7d& q) {
-  for (int i = 0; i < std::size(model_names.joints); ++i) {
+  this->state.target_angles = q;
+  for (size_t i = 0; i < std::size(model_names.joints); ++i) {
     this->sim.d->ctrl[this->sim.m->jnt_qposadr[this->ids.joints[i]]] = q[i];
   }
 }
 
 common::Vector7d FR3::get_joint_position() {
   common::Vector7d q;
-  for (int i = 0; i < std::size(model_names.joints); ++i) {
+  for (size_t i = 0; i < std::size(model_names.joints); ++i) {
     q[i] = this->sim.d->ctrl[this->sim.m->jnt_qposadr[this->ids.joints[i]]];
   }
   return q;
@@ -173,6 +151,36 @@ void FR3::set_cartesian_position(const common::Pose& pose) {
   } else {
     this->state.ik_success = false;
   }
+}
+void FR3::is_moving_callback() {
+  auto current_angles = this->get_joint_position();
+  this->state.is_moving = not this->state.previous_angles.isApprox(
+      current_angles, this->cfg.tolerance);
+}
+
+void FR3::is_arrived_callback() {
+  auto current_angles = this->get_joint_position();
+  this->state.is_arrived =
+      this->state.target_angles.isApprox(current_angles, this->cfg.tolerance);
+}
+
+void FR3::collision_callback() {
+  this->state.collision = false;
+  for (size_t i = 0; i < this->sim.d->ncon; ++i) {
+    if (this->ids.cgeom.contains(this->sim.d->contact[i].geom[0]) ||
+        this->ids.cgeom.contains(this->sim.d->contact[i].geom[0])) {
+      this->state.collision = true;
+    }
+  }
+}
+
+bool FR3::convergence_callback() {
+  /* Not moving anymore but not arrived → arm is stuck */
+  if (not this->state.is_moving and not this->state.is_arrived) {
+    return true;
+  }
+  /* Other we are done when we arrived and stopped moving */
+  return this->state.is_arrived and not this->state.is_moving;
 }
 }  // namespace sim
 }  // namespace rcs
