7d7655018e16018948122a52fae6b5926255d2cb
Pierre Krack <pierre.krack@utn.de>
2024-06-21 16:05:01 +0200

    Clean up rcsss.sim module
    It now imports FR3 from _core and deletes the mujoco model so it does
    not appear in autocompletion

--- python/rcsss/sim.py
+++ python/rcsss/sim.py
@@ -1,4 +1,5 @@
 import mujoco as mj
+from rcsss._core.sim import FR3
 from rcsss._core.sim import Sim as _Sim
 
 __all__ = ["Sim"]
@@ -9,3 +10,6 @@ class Sim(_Sim):
         self.model = mj.MjModel.from_xml_path(mjmdl)
         self.data = mj.MjData(self.model)
         super().__init__(self.model._address, self.data._address)
+
+
+del mj
