8fbf35673ca998867c3a76a6052c70042f208e00
Tobias Jülg <tobias.juelg@utn.de>
2024-06-21 12:04:56 +0200

    feat(cam): Base camera sim impl. in cpp

---
+++ src/sim/camera.h
@@ -0,0 +1,57 @@
+#ifndef RCS_CAMSIM_H
+#define RCS_CAMSIM_H
+#include <common/Pose.h>
+#include <common/Robot.h>
+#include <mujoco/mujoco.h>
+
+#include <mutex>
+
+#include "rl/mdl/JacobianInverseKinematics.h"
+#include "rl/mdl/Kinematic.h"
+#include "rl/mdl/Model.h"
+#include "sim/sim.h"
+
+namespace rcs {
+namespace sim {
+
+struct SimCameraConfig {
+  std::map<std::string, std::string> camera2id;
+  int frame_rate;
+  int resolution_width;
+  int resolution_height;
+};
+
+typedef Eigen::Matrix<uint8_t, -1, -1, 3> ColorFrame;
+
+struct FrameSet {
+  // TODO: think about who manges the memory for the frames
+  // probably simcam but where is it safed?
+  std::map<std::string, ColorFrame> color_frames;
+  float timestamp;
+};
+
+class SimCameraSet {
+ public:
+  SimCameraSet(std::shared_ptr<rcs::sim::Sim> sim, SimCameraConfig &cfg);
+  ~SimCameraSet();
+
+  int buffer_size();
+  void clear_buffer();
+
+  FrameSet get_latest_frameset();
+  FrameSet get_timestamp_frameset(float ts);
+  /** TODO: method that returns all frames within a timestamp range*/
+
+ private:
+  const SimCameraConfig cfg;
+  std::shared_ptr<Sim> sim;
+  std::vector<FrameSet> buffer;
+  std::mutex buffer_lock;
+
+  void frame_callback();
+
+  ColorFrame poll_frame(std::string camera_id);
+};
+}  // namespace sim
+}  // namespace rcs
+#endif  // RCS_CAMSIM_H
