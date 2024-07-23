ffd83ccf4779503588dbcce82f2d849886e918af
Pierre Krack <pierre.krack@utn.de>
2024-07-23 11:55:44 +0200

    Fix scene paths in pip package

--- python/rcsss/__init__.py
+++ python/rcsss/__init__.py
@@ -3,7 +3,7 @@
 from rcsss import camera, desk, sim
 from rcsss._core import __version__, common, hw
 import pathlib
-scenes = {path.stem: path for path in (pathlib.Path(__file__).parent.resolve() / "scenes").glob("*")}
+scenes = {path.stem: path / "scene.mjb"  for path in (pathlib.Path(__file__).parent.resolve() / "scenes").glob("*")}
 
 __all__ = ["__doc__", "__version__", "common", "hw", "sim", "desk", "camera", "scenes"]
 
