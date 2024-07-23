1b906c22993dc66120afff258e8b3f2a9d818549
Pierre Krack <pierre.krack@utn.de>
2024-07-23 12:00:56 +0200

    format pass

--- python/rcsss/__init__.py
+++ python/rcsss/__init__.py
@@ -1,9 +1,10 @@
 """Robot control stack python bindings."""
 
+import pathlib
+
 from rcsss import camera, desk, sim
 from rcsss._core import __version__, common, hw
-import pathlib
-scenes = {path.stem: path / "scene.mjb"  for path in (pathlib.Path(__file__).parent.resolve() / "scenes").glob("*")}
+
+scenes = {path.stem: path / "scene.mjb" for path in (pathlib.Path(__file__).parent.resolve() / "scenes").glob("*")}
 
 __all__ = ["__doc__", "__version__", "common", "hw", "sim", "desk", "camera", "scenes"]
-
