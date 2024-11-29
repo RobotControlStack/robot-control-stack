4a2a35047bc8a9860ba9b96abfba76b514a434f3
Tobias Jülg <tobias.juelg@utn.de>
2024-11-29 17:56:44 +0100

    feat(gym env): register envs in gym

--- python/rcsss/envs/factories.py
+++ python/rcsss/envs/factories.py
@@ -2,6 +2,8 @@ import logging
 from os import PathLike
 
 import gymnasium as gym
+from gymnasium.envs import EnvCreator
+import numpy as np
 import rcsss
 from rcsss import sim
 from rcsss._core.hw import FR3Config, IKController
@@ -43,7 +45,9 @@ def default_fr3_hw_gripper_cfg():
     return gripper_cfg
 
 
-def default_realsense(name2id: dict[str, str]):
+def default_realsense(name2id: dict[str, str] | None) -> RealSenseCameraSet | None:
+    if name2id is None:
+        return None
     cameras = {name: BaseCameraConfig(identifier=id) for name, id in name2id.items()}
     cam_cfg = RealSenseSetConfig(
         cameras=cameras,
@@ -229,3 +233,83 @@ def fr3_sim_env(
         env = RelativeActionSpace(env, max_mov=max_relative_movement, relative_to=relative_to)
 
     return env
+
+
+class FR3Real(EnvCreator):
+    def __call__(
+        self,
+        robot_ip: str,
+        control_mode: str = "xyzrpy",
+        delta_actions: bool = True,
+        camera_config: dict[str, str] | None = None,
+        gripper: bool = True,
+    ) -> gym.Env:
+        camera_set = default_realsense(camera_config)
+        env_rel = fr3_hw_env(
+            ip=robot_ip,
+            camera_set=camera_set,
+            control_mode=(
+                ControlMode.CARTESIAN_TRPY
+                if control_mode == "xyzrpy"
+                else ControlMode.JOINTS if control_mode == "joints" else ControlMode.CARTESIAN_TQuart
+            ),
+            robot_cfg=default_fr3_hw_robot_cfg(),
+            collision_guard=None,
+            gripper_cfg=default_fr3_hw_gripper_cfg() if gripper else None,
+            max_relative_movement=(0.2, np.deg2rad(45)) if delta_actions else None,
+            relative_to=RelativeTo.LAST_STEP,
+        )
+
+        return env_rel
+
+
+class FR3SimplePickUpSim(EnvCreator):
+    def __call__(
+        self,
+        render_mode: str = "human",
+        control_mode: str = "xyzrpy",
+        resolution: tuple[int, int] | None = None,
+        frame_rate: int = 10,
+        delta_actions: bool = True,
+    ) -> gym.Env:
+        if resolution is None:
+            resolution = (256, 256)
+
+        cameras = {
+            "wrist": SimCameraConfig(identifier="eye-in-hand_0", type=int(CameraType.fixed)),
+            "bird_eye": SimCameraConfig(identifier="bird-eye-cam", type=int(CameraType.fixed)),
+            "side": SimCameraConfig(identifier="side_view", type=int(CameraType.fixed)),
+            "right_side": SimCameraConfig(identifier="right_side", type=int(CameraType.fixed)),
+            "left_side": SimCameraConfig(identifier="left_side", type=int(CameraType.fixed)),
+            "front": SimCameraConfig(identifier="front", type=int(CameraType.fixed)),
+        }
+
+        camera_cfg = SimCameraSetConfig(
+            cameras=cameras,
+            resolution_width=resolution[0],
+            resolution_height=resolution[1],
+            frame_rate=frame_rate,
+            physical_units=True,
+        )
+        env_rel = fr3_sim_env(
+            control_mode=(
+                ControlMode.CARTESIAN_TRPY
+                if control_mode == "xyzrpy"
+                else ControlMode.JOINTS if control_mode == "joints" else ControlMode.CARTESIAN_TQuart
+            ),
+            robot_cfg=default_fr3_sim_robot_cfg(),
+            collision_guard=False,
+            gripper_cfg=default_fr3_sim_gripper_cfg(),
+            camera_set_cfg=camera_cfg,
+            max_relative_movement=(0.2, np.deg2rad(45)) if delta_actions else None,
+            relative_to=RelativeTo.LAST_STEP,
+            mjcf="fr3_simple_pick_up",
+        )
+        if render_mode == "human":
+            env_rel.get_wrapper_attr("sim").open_gui()
+
+        # TODO:
+        # - randomize location of the yellow cube
+        # - wrapper for success/failure and reward
+
+        return env_rel
