bf64b7375442a8cf2b3f9e8dfe46fb025fd87640
suman1209 <sumanrbt1997@gmail.com>
2025-01-13 18:41:57 +0100

    refactor(grasp_demo): refactored to correct linting issues

--- python/examples/grasp_demo.py
+++ python/examples/grasp_demo.py
@@ -1,11 +1,11 @@
 import logging
+import sys
 from typing import Any, cast
 
 import gymnasium as gym
 import mujoco
 import numpy as np
 from rcsss._core.common import Pose
-from rcsss._core.sim import FR3State
 from rcsss.envs.base import FR3Env, GripperWrapper
 
 logger = logging.getLogger(__name__)
@@ -26,10 +26,10 @@ class PickUpDemo:
         data = self.env.get_wrapper_attr("sim").data
 
         geom_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_GEOM, geom_name)
-        obj_pose_world_coordinates = Pose(translation=data.geom_xpos[geom_id],
-                                          rotation=data.geom_xmat[geom_id].reshape(3, 3))
-        obj_pose_robot_coordinates = self.env.unwrapped.robot.to_pose_in_robot_coordinates(obj_pose_world_coordinates)
-        return obj_pose_robot_coordinates
+        obj_pose_world_coordinates = Pose(
+            translation=data.geom_xpos[geom_id], rotation=data.geom_xmat[geom_id].reshape(3, 3)
+        )
+        return self.env.unwrapped.robot.to_pose_in_robot_coordinates(obj_pose_world_coordinates)
 
     def generate_waypoints(self, start_pose: Pose, end_pose: Pose, num_waypoints: int) -> list[Pose]:
         waypoints = []
@@ -39,9 +39,7 @@ class PickUpDemo:
         return waypoints
 
     def step(self, action: np.ndarray) -> dict:
-        re = self.env.step(action)
-        s: FR3State = self.env.unwrapped.robot.get_state()
-        return re
+        return self.env.step(action)
 
     def plan_linear_motion(self, geom_name: str, delta_up: float, num_waypoints: int = 200) -> list[Pose]:
         end_eff_pose = self.unwrapped.robot.get_cartesian_position()
@@ -59,14 +57,16 @@ class PickUpDemo:
             # calculate delta action
             delta_action = waypoints[i] * waypoints[i - 1].inverse()
             obs = self.step(self._action(delta_action, gripper))
-            ik_success = obs[-1]['ik_success']
-            if not obs[-1]['ik_success']:
+            ik_success = obs[-1]["ik_success"]
+            if not obs[-1]["ik_success"]:
                 trans_source, rot_source = waypoints[i - 1].translation(), waypoints[i - 1].rotation_rpy().as_vector()
                 trans_dest, rot_des = waypoints[i].translation(), waypoints[i].rotation_rpy().as_vector()
-                print(f"ik success: {ik_success} when attempting to move from trans: {trans_source}, rot: {rot_source}\n"
-                      f" to trans: {trans_dest} rot: {rot_des}!")
-                print(f"aborting motion!")
-                exit(-1)
+                print(
+                    f"ik success: {ik_success} when attempting to move from trans: {trans_source}, rot: {rot_source}\n"
+                    f" to trans: {trans_dest} rot: {rot_des}!"
+                )
+                print("aborting motion!")
+                sys.exit(-1)
         return obs
 
     def approach(self, geom_name: str):
@@ -100,9 +100,13 @@ def main():
                     "rcs/FR3LabPickUpSimDigitHand-v0",
                     "rcs/SimplePickUpSimDigitHand-v0"
     """
-    env = gym.make("rcs/FR3LabPickUpSimDigitHand-v0", render_mode="human", delta_actions=True,
-                   robot2_cam_pose=[0.1243549, -1.4711298, 1.2246249, -1.9944441, 0.0872650, 1.3396115, 2.1275465])
-    obs = env.reset()
+    env = gym.make(
+        "rcs/FR3LabPickUpSimDigitHand-v0",
+        render_mode="human",
+        delta_actions=True,
+        robot2_cam_pose=[0.1243549, -1.4711298, 1.2246249, -1.9944441, 0.0872650, 1.3396115, 2.1275465],
+    )
+    env.reset()
     controller = PickUpDemo(env)
     controller.pickup("yellow_box_geom")
 
