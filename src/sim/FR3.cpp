76cdd0627d57cd78bafb966c40ea68608c457f07
Tobias Jülg <tobias.juelg@utn.de>
2025-03-24 11:00:48 +0100

    fix(control): joint reset also sets controllers

--- src/sim/FR3.cpp
+++ src/sim/FR3.cpp
@@ -207,6 +207,7 @@ void FR3::set_joints_hard(const common::Vector7d& q) {
     size_t jnt_id = this->ids.joints[i];
     size_t jnt_qposadr = this->sim->m->jnt_qposadr[jnt_id];
     this->sim->d->qpos[jnt_qposadr] = q[i];
+    this->sim->d->ctrl[this->ids.actuators[i]] = q[i];
   }
 }
 
