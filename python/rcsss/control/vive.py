84d7a4afb6f972f996b04f045af0bb95b5174953
Tobias Jülg <tobias.juelg@utn.de>
2025-03-25 14:08:12 +0100

    feat: updated vive teleop to use controls and data recording

--- python/rcsss/control/vive.py
+++ python/rcsss/control/vive.py
@@ -1,12 +1,16 @@
 import logging
+import sys
 import threading
-import typing
 from enum import IntFlag, auto
 from socket import AF_INET, SOCK_DGRAM, socket
-from struct import pack, unpack
+from struct import unpack
+from time import sleep
+from rcsss.camera.realsense import RealSenseCameraSet
 
 import numpy as np
 from rcsss._core.common import RPY, Pose
+from rcsss.control.fr3_desk import FCI, Desk, DummyResourceManager
+from rcsss.control.utils import load_creds_fr3_desk
 from rcsss.envs.base import (
     ControlMode,
     GripperDictType,
@@ -21,21 +25,23 @@ from rcsss.envs.factories import (
     default_fr3_sim_gripper_cfg,
     default_fr3_sim_robot_cfg,
     default_mujoco_cameraset_cfg,
+    default_realsense,
     fr3_hw_env,
     fr3_sim_env,
 )
-
-# import matplotlib.pyplot as plt
+from rcsss.envs.wrappers import StorageWrapper
 
 
 logger = logging.getLogger(__name__)
 
 EGO_LOCK = False
-VIVE_HOST = "192.168.100.1"
+VIVE_HOST = "192.168.99.1"
 VIVE_PORT = 54321
+
 INCLUDE_ROTATION = True
 ROBOT_IP = "192.168.101.1"
 ROBOT_INSTANCE = RobotInstance.HARDWARE
+DEBUG = True
 
 
 class Button(IntFlag):
@@ -49,9 +55,10 @@ class Button(IntFlag):
 
 class UDPViveActionServer(threading.Thread):
     # seven doubles and one integer in network byte order
-    FMT = "!" + 7 * "d" + "i"
+    FMT = "!" + 7 * "d" + "i" + 6 * "d"
 
     # base transform from OpenXR coordinate system
+    # transform_from_openxr = Pose(RPY(roll=0.5 * np.pi, yaw=0)) # for second robot
     transform_from_openxr = Pose(RPY(roll=0.5 * np.pi, yaw=np.pi))
 
     def __init__(
@@ -74,6 +81,7 @@ class UDPViveActionServer(threading.Thread):
         self._ego_lock = EGO_LOCK
         self._ego_transform = Pose()
         self._env.set_origin_to_current()
+        self._step_env = False
 
     def next_action(self) -> Pose:
         transform = Pose(
@@ -94,14 +102,12 @@ class UDPViveActionServer(threading.Thread):
     def run(self):
         warning_raised = False
 
-        with socket(AF_INET, SOCK_DGRAM) as sock, socket(AF_INET, SOCK_DGRAM) as send_sock:
+        with socket(AF_INET, SOCK_DGRAM) as sock:
             sock.settimeout(2)
-            sock.setblocking(False)
             sock.bind((self._host, self._port))
-            send_sock.connect(("127.0.0.1", self._port + 1))
             while not self._exit_requested:
                 try:
-                    unpacked = unpack(UDPViveActionServer.FMT, sock.recv(7 * 8 + 4))
+                    unpacked = unpack(UDPViveActionServer.FMT, sock.recv(13 * 8 + 4))
                     if warning_raised:
                         logger.info("[UDP Server] connection reestablished")
                         warning_raised = False
@@ -130,7 +136,7 @@ class UDPViveActionServer(threading.Thread):
                             )
 
                             # Compute angle around z axis: https://stackoverflow.com/questions/21483999/using-atan2-to-find-angle-between-two-vectors
-                            rot_z = np.arctan2(x_axis_rot.translation()[1], x_axis_rot.translation()[0]) - np.arctan2(
+                            rot_z = np.atan2(x_axis_rot.translation()[1], x_axis_rot.translation()[0]) - np.atan2(
                                 x_axis.translation()[1], x_axis.translation()[0]
                             )
                             rot_z -= np.pi / 2
@@ -155,19 +161,6 @@ class UDPViveActionServer(threading.Thread):
                         # button is pressed
                         self._last_controller_pose = last_controller_pose
 
-                        # plot current offset with liveplot.py
-                        transform = Pose(
-                            translation=self._last_controller_pose.translation() - self._offset_pose.translation(),
-                            quaternion=(self._last_controller_pose * self._offset_pose.inverse()).rotation_q(),
-                        )
-                        offset = (
-                            self._ego_transform
-                            * UDPViveActionServer.transform_from_openxr
-                            * transform
-                            * UDPViveActionServer.transform_from_openxr.inverse()
-                            * self._ego_transform.inverse()
-                        )
-                        send_sock.sendall(pack(UDPViveActionServer.FMT, *offset.rotation_q(), *offset.translation(), 0))
 
                     if Button(int(unpacked[7])) & self._grp_btn and not Button(int(self._buttons)) & self._grp_btn:
                         # just pressed
@@ -189,52 +182,123 @@ class UDPViveActionServer(threading.Thread):
     def __exit__(self, *_):
         self.stop()
 
+    def stop_env_loop(self):
+        self._step_env = False
+
     def environment_step_loop(self):
-        while True:
+        self._step_env = True
+        while self._step_env:
+            if self._exit_requested:
+                self._step_env = False
+                break
             with self._resource_lock:
                 displacement = self.next_action()
             action = dict(
                 LimitedTQuartRelDictType(tquart=np.concatenate([displacement.translation(), displacement.rotation_q()]))
             )
+
             action.update(GripperDictType(gripper=self._grp_pos))
+
             with self._env_lock:
+                sleep(0.001)
                 self._env.step(action)
 
 
+def input_loop(env_rel, action_server: UDPViveActionServer, camera_set: RealSenseCameraSet):
+    while True:
+        i = input("> ")
+        match i:
+            case "help":
+                print("You can use `quit` to stop the program, `episode` to start a new episode")
+            case "quit":
+                sys.exit(0)
+            case "episode":
+                # camera_set.clear_buffer()
+                # record videos
+                video_path = env_rel.path / "videos"
+                video_path.mkdir(parents=True, exist_ok=True)
+                print(f'{env_rel.episode_count = }')
+                # camera_set.record_video(video_path, env_rel.episode_count)
+
+                thread = threading.Thread(target=action_server.environment_step_loop)
+                thread.start()
+                input("Robot is being stepped, press enter to finish and save episode.")
+                print("stopping")
+                action_server.stop_env_loop()
+                thread.join()
+                # save
+                # camera_set.stop_video()
+                env_rel.reset()
+                print("videos saved!")
+
+
 def main():
-    # if ROBOT_INSTANCE == RobotInstance.HARDWARE:
-    #     user, pw = load_creds_fr3_desk()
-    #     resource_manger = FCI(Desk(ROBOT_IP, user, pw), unlock=False, lock_when_done=False)
-    # else:
-    #     resource_manger = DummyResourceManager()
-
-    # with resource_manger:
-
     if ROBOT_INSTANCE == RobotInstance.HARDWARE:
-        env_rel = fr3_hw_env(
-            ip=ROBOT_IP,
-            control_mode=ControlMode.CARTESIAN_TQuart,
-            robot_cfg=default_fr3_hw_robot_cfg(),
-            collision_guard="lab",
-            gripper_cfg=default_fr3_hw_gripper_cfg(),
-            max_relative_movement=0.5,
-            relative_to=RelativeTo.CONFIGURED_ORIGIN,
-        )
+        user, pw = load_creds_fr3_desk()
+        resource_manger = FCI(Desk(ROBOT_IP, user, pw), unlock=False, lock_when_done=False, guiding_mode_when_done=True)
     else:
-        env_rel = fr3_sim_env(
-            control_mode=ControlMode.CARTESIAN_TQuart,
-            # control_mode=ControlMode.JOINTS,
-            robot_cfg=default_fr3_sim_robot_cfg(),
-            gripper_cfg=default_fr3_sim_gripper_cfg(),
-            camera_set_cfg=default_mujoco_cameraset_cfg(),
-            max_relative_movement=0.5,
+        resource_manger = DummyResourceManager()
+
+    with resource_manger:
+
+        if ROBOT_INSTANCE == RobotInstance.HARDWARE:
+            camera_dict = {
+                "wrist": "244222071045",
+                # "wrist": "218622278131", # new realsense
+                "bird_eye": "243522070364",
+                # "side": "243522070385",
+                "side": "244222071045",
+            }
+            # camera_set = default_realsense(camera_dict)
+            camera_set = None
+            env_rel = fr3_hw_env(
+                ip=ROBOT_IP,
+                # camera_set = camera_set,
+                # collision_guard="models/scenes/lab/teleop_scene.xml",
+                collision_guard="models/scenes/lab/scene.xml",
+                robot_cfg=default_fr3_hw_robot_cfg(),
+                control_mode=ControlMode.CARTESIAN_TQuart,
+                # control_mode=ControlMode.JOINTS,
+                gripper_cfg=default_fr3_hw_gripper_cfg(),
+                max_relative_movement=(0.5, np.deg2rad(90)),
+                # max_relative_movement=np.deg2rad(90),
             relative_to=RelativeTo.CONFIGURED_ORIGIN,
-        )
+            )
+        else:
+            env_rel = fr3_sim_env(
+                control_mode=ControlMode.CARTESIAN_TQuart,
+                # control_mode=ControlMode.JOINTS,
+                robot_cfg=default_fr3_sim_robot_cfg(),
+                collision_guard=False,
+                # mjcf="models/scenes/lab/teleop_scene.xml",
+                gripper_cfg=default_fr3_sim_gripper_cfg(),
+                # camera_set_cfg=default_mujoco_cameraset_cfg(),
+                max_relative_movement=0.5,
+                relative_to=RelativeTo.CONFIGURED_ORIGIN,
+            )
+            env_rel.get_wrapper_attr("sim").open_gui()
 
-    env_rel.reset()
+        if not DEBUG:
+            env_rel = StorageWrapper(env_rel, path="/home/juelg/code/frankcsy/record_real_christmas")
+            # ip_secondary = "192.168.102.1"
+            # with Desk.fci(ip_secondary, user, pw):
+            #     f = rcsss.hw.FR3(ip_secondary)
+            #     config = rcsss.hw.FR3Config()
+            #     f.set_parameters(config)
+            #     env_rel.get_wrapper_attr("log_files")({
+            #         "camrobot_cart.txt": str(f.get_cartesian_position()),
+            #         "camrobot_joints.txt": str(f.get_joint_position()),
+            #     })
 
-    with UDPViveActionServer(VIVE_HOST, VIVE_PORT, typing.cast(RelativeActionSpace, env_rel)) as action_server:
-        action_server.environment_step_loop()
+        env_rel.reset()
+
+        with env_rel:
+            with UDPViveActionServer(VIVE_HOST, VIVE_PORT, env_rel) as action_server:
+                if not DEBUG:
+                    input_loop(env_rel, action_server, None)
+
+                else:
+                    action_server.environment_step_loop()
 
 
 if __name__ == "__main__":
---
+++ python/rcsss/envs/wrappers.py
@@ -0,0 +1,155 @@
+import copy
+from datetime import datetime
+import os
+from pathlib import Path
+from typing import Any, SupportsFloat
+
+import gymnasium as gym
+
+import os
+
+import gymnasium as gym
+import numpy as np
+from flatten_dict import flatten
+from PIL import Image
+
+class StorageWrapper(gym.Wrapper):
+    # TODO: this should also record the instruction
+    FILE = "episode_{}.npz"
+    GIF = "{}_episode_{}_{}.gif"
+    FOLDER = "experiment_{}"
+    GIF_DURATION_S = 0.5
+
+    def __init__(self, env: gym.Env, path: str, instruction: str | None = None, description: str | None = None, record_numpy: bool = True, gif: bool = True):
+        super().__init__(env)
+        self.episode_count = 0
+        self.step_count = 0
+        self.data = {}
+        self.timestamp = str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        self.record_numpy = record_numpy
+        self.gif = gif
+
+        # make folders
+        self.path = Path(path) / self.FOLDER.format(self.timestamp)
+        Path(self.path).mkdir(parents=True, exist_ok=False)
+        if description is None:
+            # write a small description from input into file
+            description = input("Please enter a description for this experiment: ")
+        with open(self.path / "description.txt", "w") as f:
+            f.write(description)
+        if instruction is None:
+            # write instruction from input into file
+            instruction = input("Instruction: ")
+        self.language_instruction = str(instruction)
+        self.data["language_instruction"] = self.language_instruction
+        # get git commit id
+        os.system(f'git log --format="%H" -n 1 > {os.path.join(str(self.path), "git_id.txt")}')
+        # submodule git ids
+        os.system(f'git submodule status > {os.path.join(str(self.path), "git_id_submodules.txt")}')
+        # get git diff
+        os.system(f'git diff --submodule=diff > {os.path.join(str(self.path), "git_diff.txt")}')
+
+    def flush(self):
+        """writes data to disk"""
+        if len(self.data) == 0 or self.step_count == 0:
+            return
+        print(self.data.keys())
+        if self.record_numpy:
+            np.savez(self.path / self.FILE.format(self.episode_count), **self.data)
+
+        if self.gif:
+            # for key in ["side", "wrist", "bird_eye", "openvla_view"]:
+            for key in ["side", "right_side", "bird_eye", "left_side", "front"]:
+                if f"observation.frames.{key}.rgb" in self.data:
+                    imgs = []
+                    previous_timestamp = 0
+                    for idx in range(min(len(self.data[f"observation.frames.{key}.rgb"]), len(self.data["timestamp"]))):
+                        # skip images that have timestamps closer together than 0.5s
+                        img = self.data[f"observation.frames.{key}.rgb"][idx]
+                        if self.data["timestamp"][idx] - previous_timestamp < self.GIF_DURATION_S:
+                            continue
+                        previous_timestamp = self.data["timestamp"][idx]
+                        imgs.append(Image.fromarray(img))
+                    imgs[0].save(
+                        self.path / self.GIF.format(self.timestamp, self.episode_count, key),
+                        save_all=True,
+                        append_images=imgs[1:],
+                        duration=self.GIF_DURATION_S * 1000,
+                        loop=0,
+                    )
+
+        self.episode_count += 1
+        self.data = {}
+
+    def step(self, action: dict) -> tuple[Any, SupportsFloat, bool, bool, dict[str, Any]]:
+        obs, reward, terminated, truncated, info = super().step(action)
+        # write obs and action into data
+        act_obs = {"action": action, "observation": obs}
+        flattened_act_obs = flatten(copy.deepcopy(act_obs), reducer="dot")
+        # add timestamp
+        flattened_act_obs["timestamp"] = datetime.now().timestamp()
+        self.data["language_instruction"] = self.language_instruction
+        for key, value in flattened_act_obs.items():
+            if key not in self.data:
+                self.data[key] = np.expand_dims(value, axis=0)
+            else:
+                self.data[key] = np.concatenate([self.data[key], np.expand_dims(value, axis=0)], axis=0)
+        self.step_count += 1
+        return obs, reward, terminated, truncated, info
+
+    def reset(self, *, seed: int | None = None, options: dict[str, Any] | None = None) -> tuple[Any, dict[str, Any]]:
+        self.flush()
+        self.step_count = 0
+        return super().reset(seed=seed, options=options)
+
+    def close(self):
+        self.flush()
+        return super().close()
+
+    @property
+    def logger_dir(self):
+        return self.path
+
+    def log_files(self, file2content: dict[str, str]):
+        for fn, content in file2content.items():
+            with open(self.path / fn, "w") as f:
+                f.write(content)
+
+
+def listdict2dictlist(LD):
+    return {k: [dic[k] for dic in LD] for k in LD[0]}
+
+
+class RHCWrapper(gym.Wrapper):
+    """
+    Performs receding horizon control. The policy returns `pred_horizon` actions and
+    we execute `exec_horizon` of them.
+    """
+
+    def __init__(self, env: gym.Env, exec_horizon: int):
+        super().__init__(env)
+        self.exec_horizon = exec_horizon
+
+    def step(self, actions):
+        if self.exec_horizon == 1 and len(actions.shape) == 1:
+            actions = actions[None]
+        assert len(actions) >= self.exec_horizon
+        rewards = []
+        observations = []
+        infos = []
+
+        for i in range(self.exec_horizon):
+            # obs, reward, done, trunc, info = self.env.step(actions[i])
+            obs, reward, done, trunc, info = self.env.step({"xyzrpy": actions[i, :6], "gripper": actions[i, 6]})
+            observations.append(obs)
+            rewards.append(reward)
+            infos.append(info)
+
+            if done or trunc:
+                break
+
+        infos = listdict2dictlist(infos)
+        infos["rewards"] = rewards
+        infos["observations"] = observations
+
+        return obs, np.sum(rewards), done, trunc, infos
