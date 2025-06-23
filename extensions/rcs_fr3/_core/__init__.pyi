bc2cfc91734c99ceb55a77b84ad6e9ef82c55d06
Tobias Jülg <tobias.juelg@utn.de>
2025-06-23 21:48:49 +0200

    feat: added stub files for rcs_fr3

---
+++ extensions/rcs_fr3/_core/__init__.pyi
@@ -0,0 +1,19 @@
+# ATTENTION: auto generated from C++ code, use `make stubgen` to update!
+"""
+
+        Robot Control Stack Python Bindings
+        -----------------------
+
+        .. currentmodule:: _core
+
+        .. autosummary::
+           :toctree: _generate
+
+    
+"""
+from __future__ import annotations
+
+from . import hw
+
+__all__ = ["hw"]
+__version__: str = "0.4.0"
