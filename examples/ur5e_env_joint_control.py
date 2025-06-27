e5edae4bf09a1c62a4059e62c410af215393ebae
Tobias Jülg <tobias.juelg@utn.de>
2025-06-27 21:16:15 +0200

    feat: added ur5e sim example

---
+++ examples/ur5e_env_joint_control.py
@@ -0,0 +1,55 @@
+import logging
+
+import numpy as np
+from rcs.envs.base import ControlMode, RelativeTo
+from rcs.envs.creators import SimEnvCreator
+
+import rcs
+from rcs import sim
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def main():
+
+    robot_cfg = sim.SimRobotConfig()
+    robot_cfg.actuators = ["shoulder_pan", "shoulder_lift", "elbow", "wrist_1", "wrist_2", "wrist_3"]
+    robot_cfg.joints = [
+        "shoulder_pan_joint",
+        "shoulder_lift_joint",
+        "elbow_joint",
+        "wrist_1_joint",
+        "wrist_2_joint",
+        "wrist_3_joint",
+    ]
+    robot_cfg.base = "base"
+    robot_cfg.robot_type = rcs.common.RobotType.UR5e
+    robot_cfg.attachment_site = "attachment_site"
+    robot_cfg.arm_collision_geoms = []
+    env_rel = SimEnvCreator()(
+        control_mode=ControlMode.JOINTS,
+        collision_guard=False,
+        robot_cfg=robot_cfg,
+        gripper_cfg=None,
+        # cameras=default_mujoco_cameraset_cfg(),
+        max_relative_movement=np.deg2rad(5),
+        relative_to=RelativeTo.LAST_STEP,
+        mjcf=rcs.scenes["ur5e_empty_world"]["mjcf"],
+        urdf_path=rcs.scenes["ur5e_empty_world"]["urdf"],
+    )
+    env_rel.get_wrapper_attr("sim").open_gui()
+
+    for _ in range(10):
+        obs, info = env_rel.reset()
+        for _ in range(10):
+            # sample random relative action and execute it
+            act = env_rel.action_space.sample()
+            obs, reward, terminated, truncated, info = env_rel.step(act)
+            if truncated or terminated:
+                logger.info("Truncated or terminated!")
+                return
+
+
+if __name__ == "__main__":
+    main()
