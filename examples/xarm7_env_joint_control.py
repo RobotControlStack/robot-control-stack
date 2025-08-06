c38a49136fcd16dc9c041c2f4240684c84ec9438
Tobias Jülg <tobias.juelg@utn.de>
2025-08-06 19:23:36 +0200

    feat: added mjcf xarm7

---
+++ examples/xarm7_env_joint_control.py
@@ -0,0 +1,66 @@
+import logging
+from time import sleep
+
+import numpy as np
+from rcs.envs.base import ControlMode, RelativeTo
+from rcs.envs.creators import SimEnvCreator
+from rcs.envs.utils import get_tcp_offset
+
+import rcs
+from rcs import sim
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def main():
+    robot_cfg = sim.SimRobotConfig()
+    robot_cfg.actuators = [
+        "act1",
+        "act2",
+        "act3",
+        "act4",
+        "act5",
+        "act6",
+        "act7",
+    ]
+    robot_cfg.joints = [
+        "joint1",
+        "joint2",
+        "joint3",
+        "joint4",
+        "joint5",
+        "joint6",
+        "joint7",
+    ]
+    robot_cfg.base = "base"
+    robot_cfg.robot_type = rcs.common.RobotType.XArm7
+    robot_cfg.attachment_site = "attachment_site"
+    robot_cfg.arm_collision_geoms = []
+    robot_cfg.tcp_offset = get_tcp_offset(rcs.scenes["xarm7_empty_world"]["mjcf_robot"])
+    env_rel = SimEnvCreator()(
+        control_mode=ControlMode.JOINTS,
+        collision_guard=False,
+        robot_cfg=robot_cfg,
+        gripper_cfg=None,
+        # cameras=default_mujoco_cameraset_cfg(),
+        max_relative_movement=np.deg2rad(5),
+        relative_to=RelativeTo.LAST_STEP,
+        mjcf=rcs.scenes["xarm7_empty_world"]["mjb"],
+        robot_kinematics_path=rcs.scenes["xarm7_empty_world"]["mjcf_robot"],
+    )
+    env_rel.get_wrapper_attr("sim").open_gui()
+
+    for _ in range(100):
+        obs, info = env_rel.reset()
+        for _ in range(10):
+            # sample random relative action and execute it
+            act = env_rel.action_space.sample()
+            obs, reward, terminated, truncated, info = env_rel.step(act)
+            if truncated or terminated:
+                logger.info("Truncated or terminated!")
+                return
+
+
+if __name__ == "__main__":
+    main()
