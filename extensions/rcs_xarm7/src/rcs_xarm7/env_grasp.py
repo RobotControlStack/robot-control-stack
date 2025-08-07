e0b655e37edf82bc22c14f6f2b6008f2ab600af5
kennkhr <ken.nakahara.1999@gmail.com>
2025-08-07 15:08:41 +0200

    feat: grasp demo

---
+++ extensions/rcs_xarm7/src/rcs_xarm7/env_grasp.py
@@ -0,0 +1,108 @@
+import logging
+from time import sleep
+import math
+
+import numpy as np
+from rcs._core.common import RobotPlatform
+from rcs.envs.base import ControlMode, RelativeTo
+from rcs.envs.creators import SimEnvCreator
+from rcs.envs.utils import get_tcp_offset
+from rcs_xarm7.creators import RCSXArm7EnvCreator, XArm7SimEnvCreator
+from rcs.hand.tilburg_hand import THConfig
+import rcs
+from rcs import sim
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+ROBOT_IP = "192.168.1.245"
+# ROBOT_INSTANCE = RobotPlatform.SIMULATION
+ROBOT_INSTANCE = RobotPlatform.HARDWARE
+
+
+def main():
+
+    if ROBOT_INSTANCE == RobotPlatform.HARDWARE:
+        hand_cfg = THConfig(
+            calibration_file="/home/ken/tilburg_hand/calibration.json",
+            grasp_percentage=1,
+            hand_orientation="right"
+        )
+        env_rel = RCSXArm7EnvCreator()(
+            control_mode=ControlMode.JOINTS,
+            ip=ROBOT_IP,
+            hand_cfg=hand_cfg,
+            relative_to=RelativeTo.LAST_STEP,
+            max_relative_movement=None,
+        )
+    else:
+        robot_cfg = sim.SimRobotConfig()
+        robot_cfg.actuators = [
+            "act1",
+            "act2",
+            "act3",
+            "act4",
+            "act5",
+            "act6",
+            "act7",
+        ]
+        robot_cfg.joints = [
+            "joint1",
+            "joint2",
+            "joint3",
+            "joint4",
+            "joint5",
+            "joint6",
+            "joint7",
+        ]
+        robot_cfg.base = "base"
+        robot_cfg.robot_type = rcs.common.RobotType.XArm7
+        robot_cfg.attachment_site = "attachment_site"
+        robot_cfg.arm_collision_geoms = []
+        robot_cfg.tcp_offset = get_tcp_offset(rcs.scenes["xarm7_empty_world"]["mjcf_robot"])
+        env_rel = SimEnvCreator()(
+            control_mode=ControlMode.CARTESIAN_TQuat,
+            collision_guard=False,
+            robot_cfg=robot_cfg,
+            gripper_cfg=None,
+            # cameras=default_mujoco_cameraset_cfg(),
+            max_relative_movement=0.5,
+            relative_to=RelativeTo.LAST_STEP,
+            mjcf=rcs.scenes["xarm7_empty_world"]["mjb"],
+            robot_kinematics_path=rcs.scenes["xarm7_empty_world"]["mjcf_robot"],
+        )
+        env_rel.get_wrapper_attr("sim").open_gui()
+
+    env_rel.reset()
+
+    actions = [
+        # open hand
+        ( [0, math.radians(-45), 0, math.radians(15), 0, math.radians(-25), 0], 1, 2.0 ),
+        # approach
+        ( [0, math.radians(45), 0, math.radians(40), 0, math.radians(-95), 0], 1, 2.0 ),
+        # close hand
+        ( [0, math.radians(45), 0, math.radians(40), 0, math.radians(-95), 0], 0, 2.0 ),
+        # lift
+        ( [0, math.radians(15), 0, math.radians(30), 0, math.radians(-75), 0], 0, 4.0 ),
+        # put back
+        ( [0, math.radians(45), 0, math.radians(40), 0, math.radians(-95), 0], 0, 2.0 ),
+        # open hand
+        ( [0, math.radians(45), 0, math.radians(40), 0, math.radians(-95), 0], 1, 2.0 ),
+        # back to home
+        ( [0, math.radians(-45), 0, math.radians(15), 0, math.radians(-25), 0], 1, 0.0 ),
+    ]
+
+    with env_rel:
+        for joints, hand, delay in actions:
+            act = {"joints": joints, "hand": hand}
+            obs, reward, terminated, truncated, info = env_rel.step(act)
+            if truncated or terminated:
+                logger.info("Truncated or terminated!")
+                break
+            if delay > 0:
+                sleep(delay)
+
+
+
+if __name__ == "__main__":
+    main()
