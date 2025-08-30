ce53fbbbb0b31505c077f47f29666babfdf51fad
Seongjin Bien <seongjin.bien@utn.de>
2025-08-30 11:13:41 +0200

    feat: working creator

--- extensions/rcs_tacto/src/rcs_tacto/creators.py
+++ extensions/rcs_tacto/src/rcs_tacto/creators.py
@@ -1,6 +1,7 @@
 import logging
 import typing
 
+import numpy as np
 import gymnasium as gym
 from gymnasium.envs.registration import EnvCreator
 from rcs_tacto.tacto_wrapper import TactoSimWrapper
@@ -9,14 +10,15 @@ from rcs.camera.sim import SimCameraConfig
 from rcs.envs.base import ControlMode
 from rcs.envs.creators import SimTaskEnvCreator
 from rcs.envs.utils import default_sim_robot_cfg
-
+from rcs._core.common import Pose
 from rcs.sim import SimGripperConfig
+import rcs
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class FR3DigitSimplePickUpSimEnvCreator(EnvCreator):
+class FR3TactoSimplePickUpSimEnvCreator(EnvCreator):
     def __call__(  # type: ignore
         self,
         render_mode: str = "human",
@@ -49,6 +51,13 @@ class FR3DigitSimplePickUpSimEnvCreator(EnvCreator):
             for cam in cam_list
         }
         robot_cfg = default_sim_robot_cfg(scene="fr3_digit_simple_pick_up")  # id = 0 by default
+        # TODO: Figure out why feeding it the default doesn't work.
+        #       Probably because Pinocchio freaks out over all the weird tags?
+        robot_cfg.kinematic_model_path = rcs.scenes["fr3_empty_world"].mjcf_robot
+        robot_cfg.tcp_offset = Pose(
+                    translation=np.array([0.0, 0.0, 0.15]), 
+                    rotation=np.array([[0.707, 0.707, 0], [-0.707, 0.707, 0], [0, 0, 1]])
+        )
         gripper_cfg = SimGripperConfig()
 
         # the digit gripper has some custom finger collisions
@@ -74,12 +83,21 @@ class FR3DigitSimplePickUpSimEnvCreator(EnvCreator):
 
         # Append the id to keep it consistent with the model
         gripper_cfg.add_id("0")
+        random_pos_args = {"joint_name":"yellow-box-joint"}
 
-        env = SimTaskEnvCreator()(robot_cfg, render_mode, control_mode, delta_actions, cameras, **kwargs)
+        env = SimTaskEnvCreator()(robot_cfg,
+                                  render_mode,
+                                  control_mode,
+                                  delta_actions,
+                                  cameras,
+                                  gripper_cfg=gripper_cfg,
+                                  random_pos_args=random_pos_args,
+                                  **kwargs)
 
         # Here, we feed some default values for the tacto wrapper
         # that aligns with what we have in the fr3_digit_simple_pick_up
         if tacto_kwargs is None:
+            tacto_kwargs = {}
             tacto_kwargs["tacto_sites"] = ["left_tacto_pad_0", "right_tacto_pad_0"]
             tacto_kwargs["tacto_geoms"] = ["yellow_box_geom"]
             tacto_kwargs["tacto_fps"] = 60
