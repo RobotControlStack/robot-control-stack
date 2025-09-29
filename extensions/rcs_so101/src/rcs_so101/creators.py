c52b306041ff9e4a7f41a390e39db64c24936970
Seongjin Bien <seongjin.bien@utn.de>
2025-09-29 15:20:55 +0200

    cleanup: working teleop, incomplete joint setter getter

--- extensions/rcs_so101/src/rcs_so101/creators.py
+++ extensions/rcs_so101/src/rcs_so101/creators.py
@@ -3,15 +3,7 @@ from os import PathLike
 from pathlib import Path
 
 import gymnasium as gym
-from lerobot.common.robots import make_robot_from_config
-from lerobot.common.robots.so101_follower.config_so101_follower import (
-    SO101FollowerConfig,
-)
-from lerobot.common.teleoperators.so101_leader.config_so101_leader import (
-    SO101LeaderConfig,
-)
-from lerobot.common.teleoperators.so101_leader.so101_leader import SO101Leader
-from lerobot.common.teleoperators.utils import make_teleoperator_from_config
+
 from rcs.camera.hw import HardwareCameraSet
 from rcs.envs.base import (
     CameraSetWrapper,
@@ -22,7 +14,8 @@ from rcs.envs.base import (
     RobotEnv,
 )
 from rcs.envs.creators import RCSHardwareEnvCreator
-from rcs_so101.hw import SO101, S0101Gripper
+from rcs_so101.hw import SO101, SO101Config, SO101Gripper
+import rcs
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -31,45 +24,45 @@ logger.setLevel(logging.INFO)
 class RCSSO101EnvCreator(RCSHardwareEnvCreator):
     def __call__(  # type: ignore
         self,
-        id: str,
-        port: str,
-        urdf_path: str,
-        calibration_dir: PathLike | str | None = None,
+        robot_cfg: SO101Config,
+        control_mode: ControlMode,
         camera_set: HardwareCameraSet | None = None,
         max_relative_movement: float | tuple[float, float] | None = None,
         relative_to: RelativeTo = RelativeTo.LAST_STEP,
     ) -> gym.Env:
-        if isinstance(calibration_dir, str):
-            calibration_dir = Path(calibration_dir)
-        cfg = SO101FollowerConfig(id=id, calibration_dir=calibration_dir, port=port)
-        hf_robot = make_robot_from_config(cfg)
-        robot = SO101(hf_robot)  # TODO add path
-        env: gym.Env = RobotEnv(robot, ControlMode.JOINTS)
-        # env = FR3HW(env)
+        ik = rcs.common.Pin(
+            robot_cfg.kinematic_model_path,
+            robot_cfg.attachment_site,
+            urdf=robot_cfg.kinematic_model_path.endswith(".urdf"),
+        )
+        robot = SO101(robot_cfg=robot_cfg, ik=ik)
+        env: gym.Env = RobotEnv(robot, control_mode, home_on_reset=True)
 
-        gripper = S0101Gripper(hf_robot)
+        gripper = SO101Gripper(robot._hf_robot, robot)
         env = GripperWrapper(env, gripper, binary=False)
 
         if camera_set is not None:
             camera_set.start()
             camera_set.wait_for_frames()
             logger.info("CameraSet started")
-            env = CameraSetWrapper(env, camera_set)
+            env = CameraSetWrapper(env, camera_set, include_depth=True)
 
         if max_relative_movement is not None:
             env = RelativeActionSpace(env, max_mov=max_relative_movement, relative_to=relative_to)
 
         return env
 
-    @staticmethod
-    def teleoperator(
-        id: str,
-        port: str,
-        calibration_dir: PathLike | str | None = None,
-    ) -> SO101Leader:
-        if isinstance(calibration_dir, str):
-            calibration_dir = Path(calibration_dir)
-        cfg = SO101LeaderConfig(id=id, calibration_dir=calibration_dir, port=port)
-        teleop = make_teleoperator_from_config(cfg)
-        teleop.connect()
-        return teleop
+    # For now, the leader-follower teleop script uses the leader object directly
+    # and doesn't depend on an RCS-provided class.
+    # @staticmethod
+    # def teleoperator(
+    #     id: str,
+    #     port: str,
+    #     calibration_dir: PathLike | str | None = None,
+    # ) -> SO101Leader:
+    #     if isinstance(calibration_dir, str):
+    #         calibration_dir = Path(calibration_dir)
+    #     cfg = SO101LeaderConfig(id=id, calibration_dir=calibration_dir, port=port)
+    #     teleop = make_teleoperator_from_config(cfg)
+    #     teleop.connect()
+    #     return teleop
--- extensions/rcs_so101/src/rcs_so101/hw.py
+++ extensions/rcs_so101/src/rcs_so101/hw.py
@@ -1,21 +1,28 @@
+from pathlib import Path
 import typing
 
 import numpy as np
-from lerobot.common.robots.so101_follower.so101_follower import SO101Follower
+from lerobot.robots import make_robot_from_config
+from lerobot.robots.so101_follower.config_so101_follower import SO101FollowerConfig
+from lerobot.robots.so101_follower.so101_follower import SO101Follower
 
 from rcs import common, scenes
+class SO101Config(common.RobotConfig):
+    id: str = "follower"
+    port: str = "/dev/ttyACM0"
+    calibration_dir: str = "."
 
 
 class SO101(common.Robot):
-    def __init__(self, hf_robot: SO101Follower):
-        # TODO: fix the hardcoded path when merged with so101 branch
-        self.ik = common.Pin(
-            scenes["so101_empty_world"].mjcf_robot,
-            "attachment_site",
-        )
-        # ik = rcs_robotics_library._core.rl.RoboticsLibraryIK(robot_cfg.kinematic_model_path)
-        self._hf_robot = hf_robot
+    # def __init__(self, hf_robot: SO101Follower):
+    def __init__(self, robot_cfg: SO101Config, ik: common.Kinematics):
+        super().__init__()
+        self.ik = ik
+        cfg = SO101FollowerConfig(id=robot_cfg.id, calibration_dir=Path(robot_cfg.calibration_dir), port=robot_cfg.port)
+        self._hf_robot = make_robot_from_config(cfg)
         self._hf_robot.connect()
+        self._robot_config = robot_cfg
+        # ik = rcs_robotics_library._core.rl.RoboticsLibraryIK(robot_cfg.kinematic_model_path)
 
     # def get_base_pose_in_world_coordinates(self) -> Pose: ...
     def get_cartesian_position(self) -> common.Pose:
@@ -26,6 +33,7 @@ class SO101(common.Robot):
 
     def get_joint_position(self) -> np.ndarray[tuple[typing.Literal[5]], np.dtype[np.float64]]:  # type: ignore
         obs = self._hf_robot.get_observation()
+        self.obs = obs
         return typing.cast(
             np.ndarray[tuple[typing.Literal[5]], np.dtype[np.float64]],
             np.array(
@@ -41,10 +49,7 @@ class SO101(common.Robot):
         )
 
     def get_config(self):
-        a = common.RobotConfig()
-        a.robot_platform = common.RobotPlatform.HARDWARE
-        a.robot_type = common.RobotType.SO101
-        return a
+        return self._robot_config
 
     def get_state(self) -> common.RobotState:
         return common.RobotState()
@@ -74,18 +79,23 @@ class SO101(common.Robot):
                 "wrist_roll.pos": q[4],
             }
         )
+        print(q)
 
     # def to_pose_in_robot_coordinates(self, pose_in_world_coordinates: Pose) -> Pose: ...
     # def to_pose_in_world_coordinates(self, pose_in_robot_coordinates: Pose) -> Pose: ...
 
 
 # TODO: problem when we inherit from gripper then we also need to call init which doesnt exist
-class S0101Gripper(common.Gripper):
-    def __init__(self, hf_robot: SO101Follower):
+class SO101Gripper(common.Gripper):
+    def __init__(self, hf_robot: SO101Follower, robot: SO101):
+        super().__init__()
         self._hf_robot = hf_robot
+        self._robot = robot
 
     def get_normalized_width(self) -> float:
-        obs = self._hf_robot.get_observation()
+        obs = self._robot.obs
+        if obs is None:
+            return 0.0
         return obs["gripper.pos"] / 100.0
 
     # def get_config(self) -> GripperConfig: ...
