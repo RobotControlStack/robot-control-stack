c14c9119cb81c938260e74320bb024cd0bab4553
Tobias Jülg <tobias.juelg@utn.de>
2025-12-03 20:31:36 +0100

    fix: fr3 and panda init

--- extensions/rcs_fr3/src/rcs_fr3/__init__.py
+++ extensions/rcs_fr3/src/rcs_fr3/__init__.py
@@ -1,14 +1,8 @@
-import sys
-
-from rcs_fr3 import desk
+from rcs_fr3 import desk, envs
 from rcs_fr3._core import hw
 
 __all__ = [
     "desk",
     "hw",
+    "envs",
 ]
-
-if "pybind11_stubgen" not in sys.modules:
-    from rcs_panda import envs  # noqa: F401
-
-    __all__.append("envs")
--- extensions/rcs_panda/src/rcs_panda/__init__.py
+++ extensions/rcs_panda/src/rcs_panda/__init__.py
@@ -1,13 +1,7 @@
-import sys
-
+from rcs_panda import envs
 from rcs_panda._core import hw
 
 __all__ = [
     "hw",
+    "envs",
 ]
-
-# prevent gymnasium import during stubgen, which can cause a segfault
-if "pybind11_stubgen" not in sys.modules:
-    from rcs_panda import envs  # noqa: F401
-
-    __all__.append("envs")
