152e40c9f7c50a6474c7b77851dcbe1f437244fb
Tobias Jülg <tobias.juelg@utn.de>
2025-12-03 03:48:32 +0100

    ci: fix stubgen by removing gym import

--- extensions/rcs_fr3/src/rcs_fr3/__init__.py
+++ extensions/rcs_fr3/src/rcs_fr3/__init__.py
@@ -1,8 +1,14 @@
-from rcs_fr3 import desk, envs
+import sys
+
+from rcs_fr3 import desk
 from rcs_fr3._core import hw
 
 __all__ = [
     "desk",
-    "envs",
     "hw",
 ]
+
+if "pybind11_stubgen" not in sys.modules:
+    from rcs_panda import envs  # noqa: F401
+
+    __all__.append("envs")
--- extensions/rcs_panda/src/rcs_panda/__init__.py
+++ extensions/rcs_panda/src/rcs_panda/__init__.py
@@ -1,7 +1,13 @@
-from rcs_panda import envs
+import sys
+
 from rcs_panda._core import hw
 
 __all__ = [
-    "envs",
     "hw",
 ]
+
+# prevent gymnasium import during stubgen, which can cause a segfault
+if "pybind11_stubgen" not in sys.modules:
+    from rcs_panda import envs  # noqa: F401
+
+    __all__.append("envs")
