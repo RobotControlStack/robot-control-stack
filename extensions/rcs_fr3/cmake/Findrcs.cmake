c57c151aeca977c861e3bc485203abf79cd04ab3
Tobias Jülg <tobias.juelg@utn.de>
2026-06-26 12:05:55 -0700

    build: rename rcs to rcs-core
    - fix rcs package name in extensions
    - added rcs as dependency in build process
    - improved robustness of downloading assets

--- extensions/rcs_fr3/cmake/Findrcs.cmake
+++ extensions/rcs_fr3/cmake/Findrcs.cmake
@@ -2,28 +2,43 @@ if (NOT rcs_FOUND)
     if (NOT Python3_FOUND)
         set(rcs_FOUND FALSE)
         if (rcs_FIND_REQUIRED)
-            message(FATAL_ERROR "Could not find rcs. Please install rcs using pip.")
+            message(FATAL_ERROR "Could not find rcs 1. Please install rcs-core using pip.")
+        endif()
+        return()
+    endif()
+
+    execute_process(
+        COMMAND
+            ${Python3_EXECUTABLE}
+            -c
+            "import importlib.util; import pathlib; spec = importlib.util.find_spec('rcs'); print(pathlib.Path(next(iter(spec.submodule_search_locations))).resolve() if spec and spec.submodule_search_locations else '')"
+        OUTPUT_VARIABLE rcs_package_dir
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+    if (NOT rcs_package_dir)
+        set(rcs_FOUND FALSE)
+        if (rcs_FIND_REQUIRED)
+            message(FATAL_ERROR "Could not find rcs 2. Please install rcs-core using pip.")
         endif()
         return()
     endif()
 
     # Check if the include directory exists
-    cmake_path(APPEND Python3_SITELIB rcs include OUTPUT_VARIABLE rcs_INCLUDE_DIRS)
+    cmake_path(APPEND rcs_package_dir include OUTPUT_VARIABLE rcs_INCLUDE_DIRS)
     if (NOT EXISTS ${rcs_INCLUDE_DIRS})
         set(rcs_FOUND FALSE)
         if (rcs_FIND_REQUIRED)
-            message(FATAL_ERROR "Could not find rcs. Please install rcs using pip.")
+            message(FATAL_ERROR "Could not find rcs 3. Please install rcs-core using pip.")
         endif()
         return()
     endif()
 
     # Check if the library file exists
-    cmake_path(APPEND Python3_SITELIB rcs OUTPUT_VARIABLE rcs_library_path)
-    file(GLOB rcs_library_path "${rcs_library_path}/librcs.so")
+    set(rcs_library_path "${rcs_package_dir}/librcs.so")
     if (NOT EXISTS ${rcs_library_path})
         set(rcs_FOUND FALSE)
         if (rcs_FIND_REQUIRED)
-            message(FATAL_ERROR "Could not find rcs. Please install rcs using pip.")
+            message(FATAL_ERROR "Could not find rcs 4. Please install rcs-core using pip.")
         endif()
         return()
     endif()
