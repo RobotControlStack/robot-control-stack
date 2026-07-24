ee0b014aa16200536624762b27aae8741800993e
alectimison-maker <alec.timison@gmail.com>
2026-07-24 17:16:11 +0800

    fix(zed): remove hidden realsense dependency

--- extensions/rcs_zed/src/rcs_zed/utils.py
+++ extensions/rcs_zed/src/rcs_zed/utils.py
@@ -1,28 +1,21 @@
-import typing
-
+from rcs import common
 from rcs.camera.hw import CalibrationStrategy
-from rcs_realsense.calibration import FR3BaseArucoCalibration
+
 from rcs_zed.camera import ZEDCameraSet
 
-from rcs import common
 
-
-def default_zed(name2id: dict[str, str] | None) -> ZEDCameraSet | None:
+def default_zed(
+    name2id: dict[str, str] | None,
+    calibration_strategy: dict[str, CalibrationStrategy] | None = None,
+) -> ZEDCameraSet | None:
     if name2id is None:
         return None
     cameras = {
         name: common.BaseCameraConfig(identifier=id, resolution_width=1280, resolution_height=720, frame_rate=30)
         for name, id in name2id.items()
     }
-    calibration_strategy = {name: typing.cast(CalibrationStrategy, FR3BaseArucoCalibration(name)) for name in name2id}
     return ZEDCameraSet(cameras=cameras, calibration_strategy=calibration_strategy)
 
 
 def default_zed_dummy_calibration(name2id: dict[str, str] | None) -> ZEDCameraSet | None:
-    if name2id is None:
-        return None
-    cameras = {
-        name: common.BaseCameraConfig(identifier=id, resolution_width=1280, resolution_height=720, frame_rate=30)
-        for name, id in name2id.items()
-    }
-    return ZEDCameraSet(cameras=cameras)
+    return default_zed(name2id)
