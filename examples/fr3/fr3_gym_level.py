4088be8219e17f64934ba1f7b0bb96874052dc95
Tobias Jülg <tobias.juelg@utn.de>
2026-07-27 23:57:03 -0700

    chore(examples): fr3 gym level stacking

--- examples/fr3/fr3_gym_level.py
+++ examples/fr3/fr3_gym_level.py
@@ -1,7 +1,6 @@
 import logging
 
 import gymnasium as gym
-import numpy as np
 from rcs._core.common import RobotPlatform
 from rcs._core.sim import SimConfig
 from rcs.camera.sim import SimCameraSet
@@ -10,6 +9,7 @@ from rcs.envs.base import (
     ControlMode,
     CoverWrapper,
     GripperWrapper,
+    HardwareEnv,
     RelativeActionSpace,
     RelativeTo,
     RobotWrapper,
@@ -25,12 +25,11 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 """
-This script demonstrates how to control the FR3 robot in joint position control mode
-using relative movements. The robot (or its simulation) samples random relative joint movements
-in a loop.
+This script demonstrates how to control the FR3 robot in Cartesian position control mode
+using relative movements. The robot (or its simulation) moves 1cm forward and then 1cm backward
+in a loop while opening and closing the gripper.
 
 To control a real FR3 robot, install the rcs_fr3 extension (`pip install extensions/rcs_fr3`),
-change the ROBOT_INSTANCE variable to RobotPlatform.HARDWARE
 and set the FR3_IP variable to the robot's IP address. Make sure to unlock the robot's joints and
 put it into FCI mode before running this script. For a scripted way of unlocking and guiding mode see the
 fr3_direct_control.py example which uses the FCI context manager.
@@ -38,6 +37,7 @@ fr3_direct_control.py example which uses the FCI context manager.
 
 ROBOT_INSTANCE = RobotPlatform.SIMULATION
 FR3_IP = "192.168.101.1"
+USE_FRANKA_HAND = True
 
 
 def main():
@@ -54,18 +54,18 @@ def main():
             async_control=False,
         )
 
-        kinematic_model_path, attachment_site = scene.kinematics_cfg(cfg)[fr3]
+        mjmodel = scene.create_model(cfg)
+        simulation = sim.Sim(mjmodel, sim_cfg)
 
+        kinematic_model_path, attachment_site = scene.kinematics_cfg(cfg)[fr3]
         ik = rcs.common.Pin(
             kinematic_model_path,
             attachment_site,
         )
-        mjmodel = scene.create_model(cfg)
-        simulation = sim.Sim(mjmodel, sim_cfg)
 
         robot = rcs.sim.SimRobot(simulation, ik, robot_cfg)
         env_rel: gym.Env = SimEnv(simulation)
-        env_rel = RobotWrapper(env_rel, robot, ControlMode.JOINTS)
+        env_rel = RobotWrapper(env_rel, robot, ControlMode.CARTESIAN_TQuat)
 
         gripper = sim.SimGripper(simulation, gripper_cfg)
         env_rel = GripperWrapper(env_rel, gripper)
@@ -76,34 +76,61 @@ def main():
         camera_set = SimCameraSet(simulation, camera_cfgs, physical_units=True, render_on_demand=True)  # type: ignore
         env_rel = CameraSetWrapper(env_rel, camera_set, include_depth=True)  # type: ignore[arg-type]
 
-        env_rel = RelativeActionSpace(
-            env_rel,
-            max_mov=np.deg2rad(5),
-            relative_to=RelativeTo.LAST_STEP,
-        )
+        env_rel = RelativeActionSpace(env_rel, max_mov=0.5, relative_to=RelativeTo.LAST_STEP)
         env_rel = CoverWrapper(env_rel)
         env_rel.get_wrapper_attr("sim").open_gui()
     else:
+        from rcs_fr3._core import hw
         from rcs_fr3.configs import DefaultFR3HardwareEnv
+        from rcs_fr3.envs import FR3HW
 
-        env_creator = DefaultFR3HardwareEnv()
-        env_creator.ip = FR3_IP
-        hw_cfg = env_creator.config()
-        hw_cfg.control_mode = ControlMode.JOINTS
-        hw_cfg.camera_cfgs = None
-        hw_cfg.max_relative_movement = np.deg2rad(5)
-        hw_cfg.relative_to = RelativeTo.LAST_STEP
-        env_rel = env_creator.create_env(hw_cfg)
+        default_env = DefaultFR3HardwareEnv()
+        default_env.ip = FR3_IP
+        hw_cfg = default_env.config()
+
+        robot_cfg = hw_cfg.robot_cfg
+        ik = rcs.common.Pin(
+            robot_cfg.kinematic_model_path,
+            robot_cfg.attachment_site,
+            urdf=robot_cfg.kinematic_model_path.endswith(".urdf"),
+        )
+        robot = hw.Franka(robot_cfg, ik)
+
+        env_rel = HardwareEnv()
+        env_rel = RobotWrapper(
+            env_rel,
+            robot,
+            ControlMode.CARTESIAN_TQuat,
+            home_on_reset=hw_cfg.wrapper_cfg.home_on_reset,
+        )
+        env_rel = FR3HW(env_rel)
+
+        gripper_cfg = hw_cfg.gripper_cfg
+        if USE_FRANKA_HAND:
+            assert isinstance(gripper_cfg, hw.FHConfig)
+            gripper = hw.FrankaHand(gripper_cfg)
+            env_rel = GripperWrapper(env_rel, gripper, binary=hw_cfg.wrapper_cfg.binary_gripper)
+
+        env_rel = RelativeActionSpace(env_rel, max_mov=0.5, relative_to=RelativeTo.LAST_STEP)
+        env_rel = CoverWrapper(env_rel)
         input("the robot is going to move, press enter whenever you are ready")
 
+    env_rel.reset()
+
     # access low level robot api to get current cartesian position
-    print(env_rel.get_wrapper_attr("robot").get_joint_position())  # type: ignore
+    print(env_rel.get_wrapper_attr("robot").get_cartesian_position())  # type: ignore
 
     for _ in range(100):
-        obs, info = env_rel.reset()
         for _ in range(10):
-            # sample random relative action and execute it
-            act = env_rel.action_space.sample()
+            # move 1cm in x direction (forward) and close gripper
+            act = {"tquat": [0.01, 0, 0, 0, 0, 0, 1], "gripper": [0]}
+            # you can also sample an action from the action space
+            # act = env_rel.action_space.sample()
+            obs, reward, terminated, truncated, info = env_rel.step(act)
+
+        for _ in range(10):
+            # move 1cm in negative x direction (backward) and open gripper
+            act = {"tquat": [-0.01, 0, 0, 0, 0, 0, 1], "gripper": [1]}
             obs, reward, terminated, truncated, info = env_rel.step(act)
 
 
